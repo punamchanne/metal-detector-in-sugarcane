78828db5bda47499391bc8e53ca05ec88a51d41d
Punam Channe <punamchanne51@gmail.com>
2026-02-14 00:40:50 +0530

    Configure API URL for production

--- client/src/services/detectionService.js
+++ client/src/services/detectionService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/detections/';
+const API_URL = (import.meta.env.VITE_API_URL || 'http://localhost:5000') + '/api/detections/';
 
 // Get user from local storage to send token
 const getAuthDetails = () => {
